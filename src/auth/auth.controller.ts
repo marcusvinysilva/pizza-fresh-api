ed50b80f4fbb1d1a833cc16b5e2773fcd2c845c4
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-26 13:58:06 -0300

    feat(auth): add profile

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/user/entities/user.entity';
 import { AuthService } from './auth.service';
 import { LoginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
+import { LoggedUser } from './logged-user.decorator';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -15,5 +17,13 @@ export class AuthController {
   })
   login(@Body() loginDto: LoginDto): Promise<LoginResponseDto> {
     return this.authService.login(loginDto);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: 'Mostrar o usuário logado no momento',
+  })
+  profile(@LoggedUser() user: User): User {
+    return user;
   }
 }
