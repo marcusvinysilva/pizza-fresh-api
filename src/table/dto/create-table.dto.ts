8a9b68f8b002e014c1e757d3a890c8745232c0d9
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-25 22:51:46 -0300

    feat: add table dto

--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,1 +1,10 @@
-export class CreateTableDto {}
+import { IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTableDto {
+  @IsPositive()
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty()
+  number: number;
+}
