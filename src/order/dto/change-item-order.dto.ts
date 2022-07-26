e79301ca0be45a85138dc29588caa8592b1bc321
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-26 12:19:08 -0300

    feat(order): add change item order dto

---
+++ src/order/dto/change-item-order.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsUUID } from 'class-validator';
+
+export class ChangeItemOrderDto {
+  @IsUUID()
+  @ApiProperty({
+    description: 'ID do pedido que está sendo atualizado',
+    example: 'e1bc0c89-a319-44df-a6e9-db66fe7b956b',
+  })
+  orderId: string;
+
+  @IsUUID(undefined, { each: true })
+  @ApiProperty({
+    description: 'Lista com os IDs dos produtos que estão no pedido',
+    example:
+      '["04f66779-bcfa-4c5c-a140-f234138890f3", "adb96fd7-cdcf-43dc-9e1b-0c0a262111f9"]',
+  })
+  products: string[];
+}
