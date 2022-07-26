f5f5599b23301eb932f936594389832f64fddc47
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-25 22:49:45 -0300

    docs(app): add status route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get, Req } from '@nestjs/common';
-import { Request } from 'express';
 import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Request } from 'express';
 
+@ApiTags('status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+    summary: 'Status da aplicação',
+  })
   getAppStatus(@Req() req: Request): string {
     const fullUrl = req.protocol + '://' + req.get('host');
     return this.appService.getAppStatus(fullUrl);
