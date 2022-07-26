8bb4b294b7bc02da16569173ba55422dc4d9eb5f
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-26 11:09:33 -0300

    feat(user): add create

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,13 +10,19 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from './entities/user.entity';
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  @ApiOperation({
+    summary: 'Criar um usuário',
+  })
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.userService.create(createUserDto);
   }
 
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const userExists = await this.prismaService.user.findUnique({
+      where: { nickname: createUserDto.nickname },
+    });
+
+    if (userExists) {
+      throw new ConflictException(
+        'Usuário já cadastrado, informe outro nickname',
+      );
+    }
+
+    if (createUserDto.password !== createUserDto.passwordConfirm) {
+      throw new ForbiddenException('Senhas não conferem');
+    }
+
+    delete createUserDto.passwordConfirm;
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    const createdUser = await this.prismaService.user.create({
+      data: {
+        ...createUserDto,
+        password: hashedPassword,
+      },
+    });
+
+    delete createdUser.password;
+
+    return createdUser;
   }
 
   findAll() {
