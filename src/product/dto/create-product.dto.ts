7a91e6dda182e8e376d0b70d535ff90854e1e797
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-25 23:55:42 -0300

    feat(product): add product dto

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,34 @@
-export class CreateProductDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString, IsUrl } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do produto',
+    example: 'Pizza de Mussarela',
+  })
+  name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Descrição do produto',
+    example: 'Mussarela e Orégano',
+  })
+  description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @ApiProperty({
+    description: 'Preço do produto',
+    example: 35.99,
+  })
+  price: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: 'Imagem do produto',
+    example: 'https://i.imgur.com/S8zz0Oh.png',
+  })
+  image: string;
+}
