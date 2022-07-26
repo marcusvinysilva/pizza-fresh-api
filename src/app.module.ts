76e1dd7be5c08fecf8daf8c0aaf48f215291a046
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-25 22:24:13 -0300

    chore: create table resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { TableModule } from './table/table.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, TableModule],
   controllers: [AppController],
   providers: [AppService],
 })
