6ac97ee34e3fbeba0f0460f64b4dbebd8769ffe6
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-26 12:21:25 -0300

    fix(order): change field name

--- src/order/dto/change-item-order.dto.ts
+++ src/order/dto/change-item-order.dto.ts
@@ -15,5 +15,5 @@ export class ChangeItemOrderDto {
     example:
       '["04f66779-bcfa-4c5c-a140-f234138890f3", "adb96fd7-cdcf-43dc-9e1b-0c0a262111f9"]',
   })
-  products: string[];
+  productsId: string[];
 }
