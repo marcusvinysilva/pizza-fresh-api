26e4b9e8a9599efd5133b65c69f1215a05204ad9
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-26 13:55:13 -0300

    feat(auth): add login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { LoginResponseDto } from './dto/login-response.dto';
+import { LoginDto } from './dto/login.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'Fazer login com um usuário e gerar um token',
+  })
+  login(@Body() loginDto: LoginDto): Promise<LoginResponseDto> {
+    return this.authService.login(loginDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginResponseDto } from './dto/login-response.dto';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prismaService: PrismaService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(data: LoginDto): Promise<LoginResponseDto> {
+    const { nickname, password } = data;
+
+    const userExists = await this.prismaService.user.findUnique({
+      where: { nickname },
+    });
+
+    if (!userExists) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    const isHashValid = await bcrypt.compare(password, userExists.password);
+
+    if (!isHashValid) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+
+    delete userExists.password;
+
+    return {
+      token: this.jwtService.sign({ nickname }),
+      user: userExists,
+    };
+  }
+}
