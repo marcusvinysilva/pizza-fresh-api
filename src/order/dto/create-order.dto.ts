7dd7b53838d43289c080eb822584f503fa475b55
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-26 12:15:46 -0300

    feat(order): add create order dto

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,1 +1,19 @@
-export class CreateOrderDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsPositive, IsUUID } from 'class-validator';
+
+export class CreateOrderDto {
+  @IsUUID()
+  @ApiProperty({
+    description: 'ID do usuário que está criando o pedido',
+    example: 'e1bc0c89-a319-44df-a6e9-db66fe7b956b',
+  })
+  userId: string;
+
+  @IsInt()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Número da mesa que está realizando o pedido',
+    example: 1,
+  })
+  tableNumber: number;
+}
