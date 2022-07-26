05a5a8f496a9599bcade02689eb8d181078fe966
Marcus Silva <71359413+marcusvinysilva@users.noreply.github.com>
2022-07-25 23:57:17 -0300

    feat(product): add prisma

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ProductService } from './product.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { ProductController } from './product.controller';
+import { ProductService } from './product.service';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ProductController],
-  providers: [ProductService]
+  providers: [ProductService],
 })
 export class ProductModule {}
